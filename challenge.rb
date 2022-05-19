0236baac6c828ff3b8d2d46e2f25d57010678ae7
Chia Carlyle <chiacarlyle@gmail.com>
2022-05-19 04:28:11 +0100

    Morse decoder code

--- challenge.rb
+++ challenge.rb
@@ -1,62 +1,26 @@
+def decode_char(morse_char)
+  morse_code_hash = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+    '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+    '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q',
+    '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
+    '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+  morse_code_hash[morse_char]
+end
 
+def decode_word(morse_word)
+  characters = morse_word.split
+  decoded_character = ''
+  characters.each { |char| decoded_character = "#{decoded_character}#{decode_char(char)}" }
+  decoded_character
+end
 
-MORSE_DICT{
-
-    '.-' => 'a',
-    '-...' => 'b',
-    '-.-.' => 'c',
-    '-..' => 'd',
-    '.' => 'e',
-    '..-.' => 'f',
-    '--.' => 'g',
-    '....' => 'h',
-    '..' => 'i',
-    '.---' => 'j',
-    '-.-' => 'k',
-    '.-..' => 'l',
-    '--' => 'm',
-    '-.' => 'n',
-    '---' => 'o',
-    '.--.' => 'p',
-    '--.-' => 'q',
-    '.-.' => 'r',
-    '...' => 's',
-    '-' => 't',
-    '..-' => 'u',
-    '...-' => 'v',
-    '.--' => 'w',
-    '-..-' => 'x',
-    '-.--' => 'y',
-    '--..' => 'z',
-    ' ' => ' ',
-  '.----' => '1',
-  '..---' => '2',
-  '...--' => '3',
-  '....-' => '4',
-  '.....' => '5',
-  '-....' => '6',
-  '--...' => '7',
-  '---..' => '8',
-  '----.' => '9',
-  '-----' => '0'
-}.freeze
-def decode_char(char)
-    MORSE_DICT[char].capitalize
-  end
-  def decode_word(word)
-    char_list = word.split
-    result = ''
-    char_list.each { |x| result += decode_char(x) }
-    result
-  end
-
-  
-def decode(sentence)
-    word_list = sentence.split('   ')
-    result = ''
-    word_list.each { |word| result += "#{decode_word(word)} " }
-    result
-  end
-  
-  print(decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'))
-  
+def decode(morse_sentence)
+  words = morse_sentence.split('   ')
+  decoded_words = []
+  words.each { |word| decoded_words.push(decode_word(word)) }
+  decoded_words.join(' ')
+end
